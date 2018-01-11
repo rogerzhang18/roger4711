8cc16977202cbabee8310ac4dbe04768582adfe6
ROGER ZHANG <rogeryj.zhang@gmail.com>
2018-01-11 14:18:12 -0800

    lab 1

--- index.php
+++ index.php
@@ -11,8 +11,45 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
-        echo 'Hello, I am Roger.'
+        include('Student.php');
+
+        $students = array();
+        
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+        
+        $third = new Student();
+        $third->surname = "Zhang";
+        $third->first_name = "Roger (Yongjian)";
+        $third->add_email('home','rogeryj.zhang@gmail.com');
+        $third->add_grade(95);
+        $third->add_grade(99);
+        $third->add_grade(97);
+        $students['A00974011'] = $third;
+
+        ksort($students); // one of the many sort functions
+
+        foreach($students as $student)
+            echo $student->toString();
+
         ?>
     </body>
 </html>
